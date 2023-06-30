2bb10d88b34e853c3293ea75da13832f7837d15f
Luyz Castelo <luyzcastelobranco@hotmail.com>
2023-06-30 03:28:07 -0300

    feat: lógica geral do jogo

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -4,6 +4,8 @@ namespace Ivory.TesteEstagio.CampoMinado
 {
     class Program
     {
+        static int TAMANHO = 9;
+
         static void Main(string[] args)
         {
             var campoMinado = new CampoMinado();
@@ -12,6 +14,66 @@ namespace Ivory.TesteEstagio.CampoMinado
 
             // Realize sua codificação a partir deste ponto, boa sorte!
 
+            while (campoMinado.JogoStatus == 0) {
+                string[][] tabuleiroMatriz = ConstruirMatriz(campoMinado.Tabuleiro);
+                EscreverMatriz(tabuleiroMatriz);
+
+                Jogar(tabuleiroMatriz, campoMinado);
+            }
+
+            if (campoMinado.JogoStatus == 1) {
+                EscreverMatriz(ConstruirMatriz(campoMinado.Tabuleiro));
+                Console.WriteLine("Parabéns, você ganhou o jogo!");
+            } else {
+                EscreverMatriz(ConstruirMatriz(campoMinado.Tabuleiro));
+                Console.WriteLine("Game over!");
+            }
+        }
+
+        static void Jogar(string[][] tabuleiroMatriz, CampoMinado campoMinado) {
+            for (int i = 0; i < TAMANHO; i++)
+            {
+                for (int j = 0; j < TAMANHO; j++)
+                {
+                    if (EhDesconhecido(tabuleiroMatriz[i][j]) && EhSeguro(i, j, tabuleiroMatriz)) {
+                        Console.WriteLine($"Abrindo posição {i + 1} {j + 1}");
+                        campoMinado.Abrir(i + 1, j + 1);
+                        return;
+                    }
+                }
+            }
+        }
+
+        static bool EhSeguro(int linha, int coluna, string[][] tabuleiroMatriz) {
+            return true;
+        }
+
+        static bool EhDesconhecido(string tabuleiroCelula) {
+            return tabuleiroCelula == "-";
+        }
+
+        static string[][] ConstruirMatriz(string tabuleiroTexto) {
+            string[][] tabuleiroMatriz = new string[TAMANHO][];
+            string[] linhas = tabuleiroTexto.Split("\r\n");
+            for (int i = 0; i < TAMANHO; i++)
+            {
+                tabuleiroMatriz[i] = new string[TAMANHO];
+                for (int j = 0; j < TAMANHO; j++)
+                {
+                    tabuleiroMatriz[i][j] = linhas[i][j].ToString();
+                }
+            }
+
+            return tabuleiroMatriz;
+        }
+
+        static void EscreverMatriz(string[][] tabuleiroMatriz) {
+            Console.WriteLine("=================");
+            foreach (var linha in tabuleiroMatriz)
+            {
+                Console.WriteLine(String.Join(" ", linha));
+            }
+            Console.WriteLine("=================");
         }
     }
 }
